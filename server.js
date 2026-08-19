f3cece5c0f8a81faca9a5d8a1504f2ffa9beb677
kouadiokouakoujeanremarck50-cell <kouadiokouakoujeanremarck50@gmail.com>
2026-08-19 01:25:37 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('ws');
+const { WebcastPushConnection } = require('tiktok-live-connector');
+
+const app = express();
+app.use(express.static('public'));
+
+const server = http.createServer(app);
+const wss = new Server({ server });
+
+// ⚠️ REMPLACE PAR TON PSEUDO TIKTOK SANS LE @
+const TIKTOK_USERNAME = 'le_duc_3'; 
+
+let tiktokConnection = new WebcastPushConnection(TIKTOK_USERNAME);
+
+tiktokConnection.connect().then(state => {
+    console.log(`Connecté avec succès au Live TikTok !`);
+}).catch(err => {
+    console.error('Erreur de connexion :', err);
+});
+
+tiktokConnection.on('chat', data => {
+    const username = data.uniqueId;
+    const comment = data.comment;
+
+    wss.clients.forEach(client => {
+        if (client.readyState === 1) {
+            client.send(JSON.stringify({ user: username, comment: comment }));
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Serveur prêt sur le port ${PORT}`);
+});
